49cf44ef99e986281795befefb7a3cb59ccf38f0
Andrew Wheal <sicoanimal@hotmail.com>
2012-03-19 00:38:00 +0000

    added breadcrumbs class to bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -4,4 +4,5 @@ Autoloader::add_classes(array(
 	'SicoNav\Multiton' => __DIR__ . '/classes/multiton.php',
 
 	'SicoNav\Nav' => __DIR__ . '/classes/nav/nav.php',
+	'SicoNav\Breadcrumbs' => __DIR__ . '/classes/breadcrumbs/breadcrumbs.php',
 ));
